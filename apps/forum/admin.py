48af3f13ab4ca2629e329ddf1d9746690d61be57
theprajin <prazenthekiller@gmail.com>
2019-02-11 15:38:01 +0545

    second commit

--- apps/forum/admin.py
+++ apps/forum/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.forum.models import Question,UserQuestion, UserAnswer, VotedUp
 
-# Register your models here.
+
+admin.site.register(Question)
+admin.site.register(UserQuestion)
+admin.site.register(UserAnswer)
+admin.site.register(VotedUp)
+
--- apps/forum/models.py
+++ apps/forum/models.py
@@ -2,18 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Question(models.Model):
+    question = models.CharField(max_length=250)
+    
+
 class UserQuestion(models.Model):
-    questioned_by = models.ForeignKey(User, related_name='questions', on_delete= models.CASCADE, null=False)
+    questioned_by = models.ForeignKey(User, related_name='questioned_by', on_delete= models.CASCADE)
+    question = models.OneToOneField(Question, related_name='questions', on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    question = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.question
 
 
 class UserAnswer(models.Model):
-    answered_by = models.ForeignKey(User, related_name='answers', on_delete=models.CASCADE, null=False)
+    answered_by = models.ForeignKey(User, related_name='answered_by', on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now=True)
     answer = models.TextField()
@@ -23,7 +28,7 @@ class UserAnswer(models.Model):
 
 
 class VotedUp(models.Model):
-    voted_by = models.ForeignKey(User, related_name='votes', on_delete=models.DO_NOTHING)
-    to_answer = models.ForeignKey(UserAnswer, related_name='to_answers', on_delete=models.DO_NOTHING)
+    voted_by = models.ForeignKey(User, related_name='voted_by', on_delete=models.DO_NOTHING)
+    to_answer = models.ForeignKey(UserAnswer, related_name='to_answer', on_delete=models.DO_NOTHING)
     voted_on = models.DateTimeField(auto_now_add = True)
-    voted_down = models.BooleanField(default=False, null=False)
+    voted_down = models.BooleanField()
