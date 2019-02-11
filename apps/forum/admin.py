b5d22a47ac63cc77f1dd21e7ee999b515ea3ad43
theprajin <prazenthekiller@gmail.com>
2019-02-11 17:46:43 +0545

    third commit

--- apps/forum/admin.py
+++ apps/forum/admin.py
@@ -3,7 +3,21 @@ from apps.forum.models import Question,UserQuestion, UserAnswer, VotedUp
 
 
 admin.site.register(Question)
-admin.site.register(UserQuestion)
-admin.site.register(UserAnswer)
-admin.site.register(VotedUp)
+
+
+@admin.register(UserQuestion)
+class UserQuestionAdmin(admin.ModelAdmin):
+    list_display = ('description','question' ,'questioned_by', 'created_date', 'updated_date')
+    list_filter = ( 'created_date',)
+
+
+@admin.register(UserAnswer)
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('answer', 'answered_to', 'answered_by', 'created_date', 'updated_date')
+    list_filter = ('answered_to',)
+
+
+@admin.register(VotedUp)
+class VotedUpAdmin(admin.ModelAdmin):
+    list_display = ('voted_by', 'to_answer', 'voted_down', 'voted_on')
 
--- apps/forum/models.py
+++ apps/forum/models.py
@@ -1,34 +1,51 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
+
+User = get_user_model()
 
 class Question(models.Model):
     question = models.CharField(max_length=250)
-    
-
-class UserQuestion(models.Model):
-    questioned_by = models.ForeignKey(User, related_name='questioned_by', on_delete= models.CASCADE)
-    question = models.OneToOneField(Question, related_name='questions', on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    description = models.TextField()
 
     def __str__(self):
         return self.question
+    
+
+class UserQuestion(models.Model):
+    questioned_by = models.ForeignKey(User, related_name='forum_questions', on_delete= models.CASCADE)
+    question = models.ForeignKey(settings.QUESTION_MODEL, related_name='questions', on_delete=models.CASCADE)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'question descriptions'
+        unique_together = ('question',)
+
+    def __str__(self):
+        return self.description
 
 
 class UserAnswer(models.Model):
-    answered_by = models.ForeignKey(User, related_name='answered_by', on_delete=models.CASCADE)
+    answered_by = models.ForeignKey(User, related_name='forum_answers', on_delete=models.CASCADE)
+    answered_to = models.ForeignKey(settings.QUESTION_MODEL, related_name='answers', on_delete=models.CASCADE)
+    answer = models.TextField()
     created_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now=True)
-    answer = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'answers'
 
     def __str__(self):
         return self.answer
 
 
 class VotedUp(models.Model):
-    voted_by = models.ForeignKey(User, related_name='voted_by', on_delete=models.DO_NOTHING)
-    to_answer = models.ForeignKey(UserAnswer, related_name='to_answer', on_delete=models.DO_NOTHING)
-    voted_on = models.DateTimeField(auto_now_add = True)
+    voted_by = models.ForeignKey(User, related_name='votes', on_delete=models.CASCADE)
+    to_answer = models.ForeignKey(UserAnswer, related_name='to_answer', on_delete=models.CASCADE)
     voted_down = models.BooleanField()
+    voted_on = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'votes'
